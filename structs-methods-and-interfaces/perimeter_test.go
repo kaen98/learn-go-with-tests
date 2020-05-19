08755869eb806b9c8c2b3ffd5bbe2dbbb3e62322
zhaokai <zhaokai@mbp.local>
2020-05-19 18:47:52 +0800

    TableDrivenTests

--- structs-methods-and-interfaces/perimeter_test.go
+++ structs-methods-and-interfaces/perimeter_test.go
@@ -13,27 +13,18 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-
-	checkArea := func(t *testing.T, shape Shape, want float64) {
-		t.Helper()
-
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %.2f want %.2f given", got, want)
-		}	
+	areaTests := []struct{
+		shape Shape
+		want float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
 	}
 
-
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
-		want := 72.0
-		checkArea(t, rectangle, want)
-	
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		want := 314.1592653589793
-		checkArea(t, circle, want)
-	})
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %.2f want %.2f given", got, tt.want)
+		}
+	}
 }
