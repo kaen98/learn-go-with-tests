2e0eb62c19421bd6b02fc149fb78a13e4f0bddb7
zhaokai <zhaokai@mbp.local>
2020-05-20 17:52:50 +0800

    pointers-and-errors

---
+++ pointers-and-errors/pointers-and-errors.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+type Bitcoin int
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	fmt.Println("address of balance in Deposit is", &w.balance)
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
+
+type Stringer interface {
+	String() string
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	w.balance -= amount
+}
---
+++ pointers-and-errors/pointers-and-errors_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"testing"
+	"fmt"
+)
+
+func TestWallet(t *testing.T) {
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+
+		wallet.Deposit(Bitcoin(10))
+
+		got := wallet.Balance()
+
+		fmt.Println("address of balance in test is", &wallet.balance)
+
+		want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{Bitcoin(20)}
+
+		wallet.Withdraw(Bitcoin(10))
+
+		got := wallet.Balance()
+
+		want := Bitcoin(10)
+
+		
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+}
