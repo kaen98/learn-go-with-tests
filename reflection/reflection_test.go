b31272220fa42ca10c9d4fa056d514cea78fc158
zhaokai <zhaokai@mbp.local>
2020-07-02 09:50:41 +0800

    reflection

---
+++ reflection/reflection_test.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"testing"
+	"reflect"
+)
+
+type Person struct {
+	Name string
+	Profile Profile
+}
+
+type Profile struct {
+	Age int
+	City string
+}
+
+func TestWalk(t *testing.T) {
+	cases := []struct{
+		Name string
+		Input interface{}
+		ExpectedCalls []string
+	}{
+		{
+			"Struct with one string field",
+			struct {
+				Name string
+			}{"Chris"},
+			[]string{"Chris"},
+		},
+		{
+			"Struct with two string fields",
+			struct {
+				Name string
+				City string
+			}{"Chris", "London"},
+			[]string{"Chris", "London"},
+		},
+		{
+			"Struct with non string feild",
+			struct {
+				Name string
+				Age int
+			}{"Chris", 33},
+			[]string{"Chris"},
+		},
+		{
+			"Nested fileds",
+			struct {
+				Name string
+				Profile struct {
+					Age int
+					City string
+				}
+			}{
+				"Chris", struct {
+					Age int
+					City string
+				}{33, "London"},
+			},
+			[]string{"Chris", "London"},
+		},
+		{
+			"Nested fileds",
+			Person {
+				"Chris",
+				Profile	{33, "London"},
+			},
+			[]string{"Chris", "London"},
+		},
+		{
+			"Pointers to things",
+			&Person{
+				"Chris",
+				Profile	{33, "London"},
+			},
+			[]string{"Chris", "London"},
+		},
+		{
+			"Slices",
+			[]Profile{
+				{33, "London"},
+				{34, "Reykjavik"},
+			},
+			[]string{"London", "Reykjavik"},
+		},
+	}
+
+
+	for _, test := range cases {
+
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
+			}
+		})
+
+
+	}
+
+}
