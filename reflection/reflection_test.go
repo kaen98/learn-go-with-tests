56a5094d0c4b3a937ced2c10a4f4b828506f7471
zhaokai <zhaokai@mbp.local>
2020-07-03 09:31:59 +0800

    with maps

--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -92,14 +92,6 @@ func TestWalk(t *testing.T) {
 			},
 			[]string{"London", "Reykjavik"},
 		},
-		{
-			"Maps",
-			map[string]string{
-				"Foo": "Bar",
-				"Boz": "Boz",
-			},
-			[]string{"Bar", "Boz"},
-		},
 	}
 
 
@@ -116,8 +108,37 @@ func TestWalk(t *testing.T) {
 				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
 			}
 		})
-
-
 	}
+
+	t.Run("with maps", func(t *testing.T) {
+		aMap := map[string]string{
+			"Foo": "Bar",
+			"Boz": "Boz",
+		}
+
+		var got []string
+		walk(aMap, func(input string){
+			got = append(got, input)
+		})
+
+		assertContains(t, got, "Bar")
+		assertContains(t, got, "Boz")
+	})
+
+}
+
+
+func assertContains(t *testing.T, haystack []string, needle string) {
+	contains := false
+	for _, x := range haystack {
+		if x == needle {
+			contains = true
+		}
+	}
+
+	if !contains {
+		t.Errorf("exppected %+v to contain %q but it didn't", haystack, needle)
+	}
+
 
 }
